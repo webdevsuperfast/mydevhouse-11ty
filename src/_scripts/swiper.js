ab091ade519284ce6673d419cd7359f8c460bf9c
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-01 19:30:07 +0800

    Added Swiper JS file

---
+++ src/_scripts/swiper.js
@@ -0,0 +1,22 @@
+import Swiper, { Navigation } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+let slider = document.getElementsByClassName('swiper')
+
+for (var i = 0; i < slider.length; i++) {
+  let swiper = new Swiper(slider[i], {
+    modules: [Navigation],
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 0,
+    loop: true,
+    loopedSlides: 3,
+    watchSlidesVisibility: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+}
