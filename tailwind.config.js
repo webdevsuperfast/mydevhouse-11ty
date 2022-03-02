bf944aae64439787f1c7d28c93cb23f0b7bfc346
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-02 08:43:35 +0800

    Removed unneeded declaration

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,50 +23,6 @@ module.exports = {
         barlow: ['Barlow', 'sans-serif'],
         'barlow-condensed': ['Barlow Condensed', 'sans-serif'],
       },
-      fontSize: {
-        '10xl': [
-          '10rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-        '12xl': [
-          '12rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-        '14xl': [
-          '14rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-        '16xl': [
-          '16rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-        '18xl': [
-          '18rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-        '20xl': [
-          '20rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-        '22xl': [
-          '22rem',
-          {
-            lineHeight: '1',
-          },
-        ],
-      },
       backgroundImage: {
         light: 'url(../images/background-light.jpg)',
         dark: 'url(../images/background-dark.jpg)',
