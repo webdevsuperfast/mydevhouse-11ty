509398e03254458bc9860d2e1323177c000de03d
webdevsuperfast <acob.rotsen@hotmail.com>
2022-03-06 12:51:31 +0800

    Added services data

---
+++ src/_data/services.js
@@ -0,0 +1,20 @@
+const { data } = require('autoprefixer')
+const graphqlQuery = require('../_utils/graphql')
+
+const getServices = async () => {
+  const data = await graphqlQuery({
+    query: `query {
+      services {
+        nodes {
+          slug
+          title
+          id
+        }
+      }
+    }`,
+  })
+
+  return data.services.nodes
+}
+
+module.exports = getServices
