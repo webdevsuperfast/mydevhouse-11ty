9e2e36493124045377bc791504389f6e29fdd3a2
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-09 17:57:24 +0800

    Removed unneeded line of codes

--- src/_scripts/locomotive.js
+++ src/_scripts/locomotive.js
@@ -1,10 +1,6 @@
 import LocomotiveScroll from 'locomotive-scroll'
 
-import sheet from '../../node_modules/locomotive-scroll/dist/locomotive-scroll.css' assert { type: 'css' }
-document.adoptedStyleSheets = [sheet]
-
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
-  direction: 'horizontal',
 })
