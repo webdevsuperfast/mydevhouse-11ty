dca128c3bf785950d918d0163ef549f39bb0929e
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-15 15:40:45 +0800

    Added pages data

---
+++ src/_data/pages.js
@@ -0,0 +1,30 @@
+const graphqlQuery = require('../_utils/graphql')
+
+async function getPages() {
+  const data = await graphqlQuery({
+    query: `query {
+      pages(
+        where: {status: PUBLISH, nameIn: ["testimonials", "about-us", "portfolio", "contact", "services"]}
+      ) {
+        nodes {
+          seo {
+            title
+            metaKeywords
+            opengraphTitle
+            metaDesc
+            focuskw
+            readingTime
+          }
+          pageId
+          content(format: RAW)
+          slug
+          title
+        }
+      }
+    }`,
+  })
+
+  return data.pages.nodes
+}
+
+module.exports = getPages
