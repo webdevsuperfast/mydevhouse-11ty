01cfa363d5dc3db7fa01d75039fe4aa7e5fe6632
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-20 20:36:38 +0800

    Added new data file

---
+++ src/_data/about.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    title: 'Our Team',
+    slug: 'our-team',
+    url: '#our-team',
+  },
+  {
+    title: 'Our Process',
+    slug: 'our-process',
+    url: '#our-process',
+  },
+  {
+    title: 'Our Services',
+    slug: 'our-services',
+    url: '#our-services',
+  },
+  {
+    title: 'Testimonials',
+    slug: 'testimonials',
+    url: '#testimonials',
+  },
+]
