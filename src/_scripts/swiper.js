d482c384ba1577aa6d2ac85b8f2618dae03c2e92
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-21 10:52:14 +0800

    Testing `creative` effect, changed configuration

--- src/_scripts/swiper.js
+++ src/_scripts/swiper.js
@@ -3,26 +3,41 @@ import Swiper, { Navigation } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/navigation'
 
-let slider = document.getElementsByClassName('swiper')
+const slider = document.getElementsByClassName('swiper')
 
-for (var i = 0; i < slider.length; i++) {
-  let swiper = new Swiper(slider[i], {
-    modules: [Navigation],
-    slidesPerView: 1,
-    centeredSlides: true,
-    spaceBetween: 0,
-    loop: true,
-    watchSlidesVisibility: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+$params = {
+  slidesPerView: 1,
+  modules: [Navigation],
+  centeredSlides: true,
+  spaceBetween: 0,
+  loop: true,
+  watchSlidesVisibility: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      translate: [0, 0, -400],
+      scale: '75%',
+      opacity: '75%',
     },
-    autoplay: {
-      delay: 2000,
+    next: {
+      translate: ['100%', 0, 0],
+      scale: '75%',
+      opacity: '75%',
     },
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-  })
+  },
+}
+
+for (let i = 0; i < slider.length; i++) {
+  if (slider[i].classList.contains('featured-portfolio')) {
+    const swiper = new Swiper(slider[i], $params)
+  } else {
+    const swiper = new Swiper(slider[i], {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    })
+  }
 }
